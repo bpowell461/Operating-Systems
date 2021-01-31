0458ebefbd248994f803eb6ac3e1dc6596268db1
Brad Powell <bpowell461@gmail.com>
2021-01-31 16:36:17 -0500

    Project 1 Added

---
+++ Pyramid/scheme.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+int N;
+
+if (argc == 1)
+{
+  N = 0;
+}
+else
+{
+  N = atoi(argv[1]);
+}
+
+if (N < 1)
+{ 
+	N = 1;
+}
+if (N > 5)
+{ 
+	printf("Error: Out of Range!");
+	N = 0;
+}
+
+printf("N: %d\n\n",N);
+
+printf("PID\tPPID\n\n");
+
+if(N!=0)
+{
+int i;
+for (i=0; i< N; i++)
+    fork();
+}
+printf("%d\t%d\n",getpid(),getppid());
+
+sleep(1);
+
+printf("All Done.\n");
+
+sleep(1);
+return 0;
+}
+
